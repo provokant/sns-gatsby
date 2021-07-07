e85c3d05a17b1e7af5a5efa567611dd551e4bc50
Michael Czechowski <mail@dailysh.it>
2021-07-07 19:31:55 +0200

    add claims (7m)

--- src/components/claims.js
+++ src/components/claims.js
@@ -1,12 +1,12 @@
 import { Section, SectionTitle, SubTitle } from "./layout";
 import * as React from "react";
-import { AcademicCapIcon, CashIcon, GlobeAltIcon, LightningBoltIcon, ScaleIcon } from "@heroicons/react/outline";
+import { ExclamationCircleIcon } from "@heroicons/react/solid";
 import tw, { GlobalStyles } from "twin.macro";
 
 export const Claims = () => {
   return (
     <>
-      <GlobalStyles/>
+      <GlobalStyles />
       <Section>
         <a name={"claims"} id={"claims"} href={"#claims"}>
           <span tw="sr-only">Studieren nicht Stagnieren!</span>
@@ -17,7 +17,7 @@ export const Claims = () => {
               <SubTitle>Unsere Forderungen</SubTitle>
               <SectionTitle>
                 Wir fordern vom Ministerium für Wissenschaft, Forschung und Kunst Baden-Württemberg <span
-                tw={"text-magenta"}>angemessene Studienbedingungen</span>
+                  tw={"text-magenta"}>angemessene Studienbedingungen</span>
               </SectionTitle>
             </div>
 
@@ -27,7 +27,7 @@ export const Claims = () => {
                   <div key={index} tw="relative">
                     <dt>
                       <div tw="absolute flex items-center justify-center h-12 w-12 rounded-md text-white bg-magenta">
-                        <item.icon tw="h-6 w-6" aria-hidden="true"/>
+                        <item.icon tw="h-6 w-6" aria-hidden="true" />
                       </div>
                     </dt>
                     <dd tw="ml-16 text-lg leading-6 font-medium text-gray-900">{item.claim}</dd>
@@ -45,27 +45,37 @@ export const Claims = () => {
 const listItems = [
   {
     claim:
-      "Wir fordern ein integratives Konzept der universitären Idee für die digitalen Angebote! Die Universität lebt auch außerhalb des Hörsaals und Diskurse lassen sich nur sehr schwer oder überhaupt nicht in Videokonferenzen führen.",
-    icon: LightningBoltIcon,
+      "Wir fordern konkrete und planungssichere Perspektiven — sei es für die Öffnung der Hochschulen, für die Konzeption eines Hybridsemesters oder für die Eingliederung von Erstsemester im Studium oder Letztsemester auf dem Arbeitsmarkt!",
+    icon: ExclamationCircleIcon,
   },
   {
     claim:
-      "Wir fordern konkrete Perspektiven — sei es für die Öffnung der Akademien, Hochschulen und Universitäten, für die Eingliederung von Erstsemester im Studium oder Letztsemester auf dem Arbeitsmarkt!",
-    icon: AcademicCapIcon,
+      "Wir fordern einen entschiedenen Ausbau und Förderung der psychosozialen Beratungs- und Hilfsangebote, auch nach der Pandemie!",
+    icon: ExclamationCircleIcon,
+  },
+  {
+    claim:
+      "Wir fordern ein integratives Konzept der digitalen Hochschule! Die Hochschule lebt auch außerhalb des Hörsaals und Diskurse lassen sich nur deutlich reduziert  in Videokonferenzen führen!",
+    icon: ExclamationCircleIcon,
   },
   {
     claim:
       "Wir fordern die Aussetzung der Gebühren für alle Studierenden, vor allem aber für die, die sich im internationalen oder zweiten Studium befinden!",
-    icon: GlobeAltIcon,
+    icon: ExclamationCircleIcon,
   },
   {
     claim:
-      "Wir fordern vernünftige, sozial angemessene finanzielle Unterstützung für Studierende!",
-    icon: CashIcon,
+      "Wir fordern vernünftige, unbürokratische und sozial angemessene finanzielle Unterstützung für Studierende!",
+    icon: ExclamationCircleIcon,
   },
   {
     claim:
-      "Wir fordern die Anpassung der Prüfungsordnungen auf die besonderen Bedingungen der Pandemie!",
-    icon: ScaleIcon,
+      "Wir fordern die sozialverträgliche und integrative Anpassung der Prüfungsordnungen und Fristen auf die besonderen Bedingungen der Pandemie!",
+    icon: ExclamationCircleIcon,
+  },
+  {
+    claim:
+      "Wir fordern maßgebliche Beteiligung und Zusammenarbeit mit der Politik in der Überwindung der pandemiebedingten Herausforderungen!",
+    icon: ExclamationCircleIcon,
   }
 ]
