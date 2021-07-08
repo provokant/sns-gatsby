963b644831609d4e9e782fb371b7163467e005c5
Michael Czechowski <mail@dailysh.it>
2021-07-09 00:05:31 +0200

    add banner (8m)

---
+++ src/components/partner.js
@@ -0,0 +1,19 @@
+import * as React from "react";
+import tw from "twin.macro";
+import { Container, Paragraph, Section } from "./layout";
+
+export const Partner = () => {
+  return (
+    <Section>
+      <Container>
+        <Posts>
+          <PostItem>
+            <Card>
+              {/* < */}
+            </Card>
+          </PostItem>
+        </Posts>
+      </Container>
+    </Section>
+  )
+}
